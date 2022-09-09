94fc9255921429135fba19f0e0c0f06aaad3639e
‘TheGreatWizard16’ <horladimeji10@gmail.com>
2022-09-08 18:43:22 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints and and that piece of art is useful
+ * Return: 1 if sucessful
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
