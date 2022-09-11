22fe652196a9a345e58a3e2a056e904003235d1e
‘TheGreatWizard16’ <horladimeji10@gmail.com>
2022-09-11 09:37:35 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,11 @@
 
 /**
  * main - Main program
- *
- * Return: 1
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Karpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
