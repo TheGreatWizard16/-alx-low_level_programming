780718a4c4238607efe1c3959a983334fc2e1a4d
‘TheGreatWizard16’ <horladimeji10@gmail.com>
2022-09-13 08:39:25 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
-/** main - prints _putchar, followed by a new line.
+/** main - prints putchar
  * Return: Always 0 (Success) 
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -14,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('/n');
+
 	return (0);
 }
